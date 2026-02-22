82bc9d6b949b30520375737dce17f92e58aca4e3
Андрей <Kovalc678@gmail.com>
2026-02-22 21:32:29 +0200

    Add: extend admin panel

--- hotel/admin.py
+++ hotel/admin.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
-from .models import RoomCategory, Amenity, Room
+from .models.room import Room
+from .models.room_category import RoomCategory
+from .models.amenity import Amenity
 
-admin.site.register(RoomCategory)
-admin.site.register(Amenity)
-admin.site.register(Room)
+@admin.register(RoomCategory)
+class RoomCategoryAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Amenity)
+class AmenityAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Room)
+class RoomAdmin(admin.ModelAdmin):
+    list_display = ('title', 'price', 'category', 'is_available')
+    list_filter = ('category', 'is_available')
+    search_fields = ('title', 'description')
