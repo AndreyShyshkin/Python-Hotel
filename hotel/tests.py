f4da87ecefb43cae14a3fd4b3b40e6e710215930
Mariia Honzhak <mariia.gonzhak@gmail.com>
2026-02-26 12:27:25 +0200

    Add category and availability filters to RoomListView

--- hotel/tests.py
+++ hotel/tests.py
@@ -14,39 +14,39 @@ class SeedRoomsCommandTest(TestCase):
         call_command('seed_rooms', stdout=out)
         self.assertEqual(Room.objects.count(), 5)
 
-def test_seed_creates_categories(self):
+    def test_seed_creates_categories(self):
         call_command('seed_rooms', stdout=StringIO())
         categories = list(RoomCategory.objects.values_list('name', flat=True))
         for name in ['Стандарт', 'Люкс', 'Сюїт', 'Апартаменти']:
             self.assertIn(name, categories)
 
-def test_seed_creates_amenities(self):
+    def test_seed_creates_amenities(self):
         call_command('seed_rooms', stdout=StringIO())
         self.assertGreaterEqual(Amenity.objects.count(), 1)
 
-def test_seed_is_idempotent(self):
+    def test_seed_is_idempotent(self):
         """Повторний запуск не дублює номери"""
         call_command('seed_rooms', stdout=StringIO())
         call_command('seed_rooms', stdout=StringIO())
         self.assertEqual(Room.objects.count(), 5)
 
-def test_seed_reset_flag(self):
+    def test_seed_reset_flag(self):
         """--reset очищає і пересіює"""
         call_command('seed_rooms', stdout=StringIO())
         call_command('seed_rooms', '--reset', stdout=StringIO())
         self.assertEqual(Room.objects.count(), 5)
 
-def test_rooms_have_amenities(self):
+    def test_rooms_have_amenities(self):
         call_command('seed_rooms', stdout=StringIO())
         for room in Room.objects.all():
             self.assertGreater(room.amenities.count(), 0, f'{room.title} has no amenities')
 
-def test_rooms_have_category(self):
+    def test_rooms_have_category(self):
         call_command('seed_rooms', stdout=StringIO())
         for room in Room.objects.all():
             self.assertIsNotNone(room.category)
 
-def test_rooms_have_price(self):
+    def test_rooms_have_price(self):
         call_command('seed_rooms', stdout=StringIO())
         for room in Room.objects.all():
             self.assertGreater(room.price, 0)
@@ -65,3 +65,14 @@ class RoomListViewTest(TestCase):
     def test_room_list_contains_rooms(self):
         response = self.client.get(reverse('room_list'))
         self.assertEqual(response.context['total'], 5)
+
+    def test_filter_by_category(self):
+        cat = RoomCategory.objects.get(name='Стандарт')
+        response = self.client.get(reverse('room_list'), {'category': cat.id})
+        for room in response.context['rooms']:
+            self.assertEqual(room.category.name, 'Стандарт')
+
+    def test_filter_by_availability(self):
+        response = self.client.get(reverse('room_list'), {'available': 'on'})
+        for room in response.context['rooms']:
+            self.assertTrue(room.is_available)
