05dec7e669871b6c110b71eb3d5095da0be29056
Mariia Honzhak <mariia.gonzhak@gmail.com>
2026-02-26 12:20:42 +0200

    Add basic test for seed_rooms management command

--- hotel/tests.py
+++ hotel/tests.py
@@ -1,3 +1,15 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from django.core.management import call_command
+from io import StringIO
 
-# Create your tests here.
+from hotel.models import Room, RoomCategory, Amenity
+
+
+class SeedRoomsCommandTest(TestCase):
+    """Тест management command seed_rooms"""
+
+    def test_seed_creates_rooms(self):
+        out = StringIO()
+        call_command('seed_rooms', stdout=out)
+        self.assertEqual(Room.objects.count(), 5)
