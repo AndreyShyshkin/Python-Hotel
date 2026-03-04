448717c2f655156ce598867835c483e2bf79a736
AndreyShyshkin <shyshkinandrey06@gmail.com>
2026-03-04 20:06:20 +0200

    fix: add merge migration to resolve conflicting 0002 migrations

---
+++ hotel/migrations/0003_merge_0002.py
@@ -0,0 +1,12 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('hotel', '0002_alter_amenity_options_alter_room_options_and_more'),
+        ('hotel', '0002_subscription_notificationlog'),
+    ]
+
+    operations = [
+    ]
